f3cc6ac3dccddba9dcd01a074fda58598dcd40a4
khaledurrashid2@gmail.com <khaledurrashid2@gmail.com>
2026-04-01 17:14:47 +0600

    setup project with tailwindCSS and DaisyUI

---
+++ digitools/src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <div className="p-10">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">DaisyUI Card</h2>
+          <p>If you see this styled nicely, DaisyUI works!</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">OK</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
